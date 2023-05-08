fcf704fa9b7c9807823cc911b50b59970be69e1d
jiaji004 <jiaji200324@gmail.com>
2023-05-08 16:02:30 +0800

    test

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test {
+	public static void main(String[] args) {
+		int a = 13;
+		int b = 27;
+		int c = 15;
+		System.out.println("a * b = " + (a * b));
+		System.out.println("b % a = " + (b % a));
+		System.out.println("a / b = " + (a / b));
+		System.out.println("a % c = " + (a % c));
+		System.out.println("a / c = " + (a / c));
+		System.out.println("c / b = " + (c / b));
+	}
+}
